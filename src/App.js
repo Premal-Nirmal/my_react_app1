da2b6ef9958c1f587a2cb2d55f23c30a78446aca
Premal Nirmal <premal_007@gmail.com>
2020-12-08 14:20:07 +0530

    Added Persion.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>Hi, this is react app</h1>
+       <p>This is really working</p>
+       <Person />
       </div>
     );
 
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Person extends Component{
+    render() {
+        return(
+            <p>Hi this is a person...</p>
+        );
+    }
+}
+
+export default Person;
