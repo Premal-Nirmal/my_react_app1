b16edbe8bc38b1cba633c517e064b836daf633e9
Premal Nirmal <premal_007@gmail.com>
2020-12-15 16:30:37 +0530

    Added state code

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    person_arr: [
+      {name: "Premal", age: "29"},
+      {name: "Shiv", age: "28"},
+      {name: "Kajal", age: "23"}
+    ]
+  }
   render() {
     return (
       <div className="App">
        <h1>Hi, this is react app</h1>
        <p>This is really working</p>
-       <Person name="Premal" age ="29" />
-       <Person name="Shiv" age ="28" > My Hobbies: Racing </Person>
-       <Person name="Kajal" age ="23" />
+       <button>Change</button>
+       <Person name = {this.state.person_arr[0].name} age = {this.state.person_arr[0].age} />
+       <Person name = {this.state.person_arr[1].name} age = {this.state.person_arr[1].age} > My Hobbies: Racing </Person>
+       <Person name = {this.state.person_arr[2].namee} age = {this.state.person_arr[2].age} />
 
       </div>
     );
